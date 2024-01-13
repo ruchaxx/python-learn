8c72bd76514f5a8742801eae87eae23a7cb451d0
ruchaxx <ruchapuranik00@gmail.com>
2024-01-13 15:46:40 +0530

    log attached in mail

--- kubernetees/service-logs.py
+++ kubernetees/service-logs.py
@@ -1,10 +1,43 @@
 import sys
 from kubernetes import client, config
-
+import smtplib
+import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
 
 
 config.load_kube_config()
 api_instance = client.CoreV1Api()
+
+find_error = False
+
+def send_mail(podlist):
+      smtp_obj = smtplib.SMTP('smtp-mail.outlook.com',587)
+      #smtp_obj = smtplib.SMTP('smtp.gmail.com',587)
+      smtp_obj.ehlo()
+      smtp_obj.starttls()
+      email = 'puranik_rucha@outlook.com'
+      password = getpass.getpass('Enter Password : ')
+      smtp_obj.login(email,password)
+      msg = MIMEMultipart()
+      msg['From'] = email
+      msg['To'] = email
+      msg['Subject'] = service_name + ' Logs'
+      body = service_name + ' logs are attached'
+      msg.attach(MIMEText(body, 'plain'))
+
+      ## ATTACHMENT PART OF THE CODE IS HERE
+      for pod in podlist:
+         attachment = open(pod+'.txt', 'rb')
+         part = MIMEBase('application', "octet-stream")
+         part.set_payload((attachment).read())
+         encoders.encode_base64(part)
+         part.add_header('Content-Disposition', "attachment; filename= %s" % pod+'.txt')
+         msg.attach(part)
+      smtp_obj.send_message(msg)
+      smtp_obj.quit()
 
 
 
@@ -30,3 +63,16 @@ for pod in podlist:
    pod_logs = api_instance.read_namespaced_pod_log(name=pod, namespace=namespace)
    with open(pod+'.txt',mode='w') as f:
          f.write(pod_logs)
+   with open(pod+'.txt',mode='r') as f:
+       lines = f.read()
+   #print("\n pod of {} lines {}".format(pod,lines))
+   if lines.find('ERROR') != -1:
+       print("in if")
+       find_error = True
+       break
+   else:
+       print("in else")
+
+if find_error == True:
+    send_mail(podlist)
+    print("\nEmail sent sussesfully")
